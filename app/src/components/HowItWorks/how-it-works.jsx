e35a581882c9ab7dcbffb7dfa66e442fcfa466a9
rw4n99 <rowangodfrey2@gmail.com>
2024-08-15 12:33:03 +0100

    Added style for HowItWorksSection

--- app/src/components/HowItWorks/how-it-works.jsx
+++ app/src/components/HowItWorks/how-it-works.jsx
@@ -1,11 +1,11 @@
 import React from 'react'; // Import React to use JSX
-//import styles from './HowItWorksSection.module.css'; // Import your CSS module for styling
+import styles from './HowItWorksSection.module.css'; // Import your CSS module for styling
 
 // Define the HowItWorksSection component
 
 function HowItWorksSection () {
   return(
-  <section class="how-it-works">
+  <section className="how-it-works">
             <div class="step">
                 <h4>Give us a call ...</h4>
                 <p>Call us and book in a "Design Consultation" on a date and time to suit you.</p>
