ec571716b5b63dc0ce10b0b4783751ccc875647f
Paulina G <paulinag@Paulinas-MacBook-Pro.local>
2024-08-20 11:32:44 +0100

    small form into added email and phone number

--- app/bookings/components/ContactForm/ContactForm.jsx
+++ app/bookings/components/ContactForm/ContactForm.jsx
@@ -22,6 +22,19 @@ export default function ContactForm() {
                 <input type='text' id='city' name='city' />
             </div>
         </fieldset>
+        <fieldset>
+            <legend>
+            Contact Information
+            </legend>
+            <div className="phoneNumber">
+            <label for="phoneNumber"> Phone Number:</label>
+            <input type='text id=number'/>
+            </div>
+            <div className="emailAddress">
+            <label for="Email">Email:</label>
+            <input type='text'/>
+            </div>
+        </fieldset>
     </form>
 
     )
