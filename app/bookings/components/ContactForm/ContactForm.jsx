b7296bb4f95188435f1cac7b5058847ddef4acb6
rw4n99 <rowangodfrey2@gmail.com>
2024-08-20 11:15:30 +0100

    Added personal information form

--- app/bookings/components/ContactForm/ContactForm.jsx
+++ app/bookings/components/ContactForm/ContactForm.jsx
@@ -5,8 +5,22 @@ export default function ContactForm() {
             <legend>
                 Personal information
             </legend>
-            <label for="name">Full Name:  </label>
-            <input type='text' id='name' name='name' required minlength="4" maxlength="8" size="10" />
+            <div className="name">
+                <label for="name">Full Name:  </label>
+                <input type='text' id='name' name='name' required minlength="4" maxlength="8" size="10" />
+            </div>
+            <div className="postcode">
+                <label for="postcode">Postcode:  </label>
+                <input type='text' id='postcode' name='postcode' />
+            </div>
+            <div className="streetName">
+                <label for="streetName">House number and Street name:  </label>
+                <input type='text' id='streetName' name='streetName' />
+            </div>
+            <div className="city">
+                <label for="city">City:  </label>
+                <input type='text' id='city' name='city' />
+            </div>
         </fieldset>
     </form>
 
